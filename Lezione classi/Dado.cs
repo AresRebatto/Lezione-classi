dfcb52d150a548f7a0d7b585341e4c3ac71f8340
AresRebatto <aresrebatt@gmail.com>
2023-04-28 22:27:57 +0200

    Aggiungere i file di progetto.

---
+++ Lezione classi/Dado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lezione_classi
+{
+    class Dado
+    {
+        //attributi
+        private string _colore { get; set; }
+        private int _numeroFacce;
+        static private int n = 4; //Utilizzare static implica che l'attributo si riferisca alla classe e non all'oggeto. E' come avere una costante
+
+        public Dado(string colore, int nFacce)//Costruttore. Ha lo stesso nome della classe
+        {
+            this._colore = colore; //this si riferisce all'oggetto corrente: evita incomprensioni dovute ai nomi
+            this._numeroFacce = nFacce;
+        }
+
+        public Dado() //Overload: la possibilità di creare più costruttori a patto che la firma(ovvero i paramentri richiesti) siano differenti
+        {
+            this._colore = "undefined";
+            this._numeroFacce = 6;
+        }
+
+        //I metodi sono azioni che possono compiere gli oggetti
+        public int LancioDadi()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, _numeroFacce+1);//1 a 6. rnd.Next(1, 7);
+        }  
+
+        static public void Numero()
+        { Console.WriteLine(n); }
+
+
+        
+
+    }
+
+}
---
+++ Lezione classi/Program.cs
@@ -0,0 +1,12 @@
+﻿//prenderemo tutti 10<----fax
+
+using Lezione_classi;
+
+Dado dado = new Dado();//System.Int32 (int) è un classe/tipo di dato
+Dado secondoDado = new Dado("rosso", 8);//Utilizza il primo costruttore dove sono richiesti dei parametri
+
+Console.WriteLine($"Il primo dado ha valore: {dado.LancioDadi()}");
+Console.WriteLine("Il primo dado ha valore: " + secondoDado.LancioDadi());
+
+Dado.Numero();
+
